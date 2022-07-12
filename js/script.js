9aaff481397759600a46d01cacbe149a83af7a60
JossTorres91 <ingjmtorres086@gmail.com>
2022-07-11 21:34:43 -0500

    Realización de ejercicios en script.js y creación de alert.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
-console.log("Hello World");
+var a = 5;
+var b = "7";
+var c = a * a;
+var d = a * b;
+var e = a + (a * b);
+var f = "JavaScript is awesome";
+
+const yearOfBirth = 1991;
+console.log(yearOfBirth);
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f.toUpperCase());
+
+var a = 15;
+console.log(a);
+
+console.log("Hello World from script.js archive".toUpperCase());
+
+var z = true;
+var y = false;
+
+console.log(z);
+console.log(y);
+//suma de los valores booleanos
+/*Comentarios largos */
+var x = z + y;
+
+console.log(typeof x);
